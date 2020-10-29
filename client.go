84f1e5202db78a63371a96ce55cb356314543f28
jamosully <72453455+jamosully@users.noreply.github.com>
2020-10-29 10:25:06 +0000

    Update client.go

--- client.go
+++ client.go
@@ -29,7 +29,7 @@ func write(conn *net.Conn) {
 
 func main() {
 	// Get the server address and port from the commandline arguments.
-	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
+	addrPtr := flag.String("ip", "172.31.85.147:8030", "IP:port string to connect to")
 	flag.Parse()
 	//TODO Try to connect to the server
 	conn, _ := net.Dial("tcp", *addrPtr)
